3eef5bda4b5c7ab4ea1effb659b40e831fffc814
DHINAKARAN-DGS <dhinakarandgs23@gmail.com>
2023-05-12 11:04:18 +0530

    finalCode

---
+++ build/precache-manifest.999cd0877dda42db4f2de18897e879ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7266c0a56d7e8b7fcedc3882031814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73a33ce032f7d452b952",
+    "url": "/static/css/2.6f44476b.chunk.css"
+  },
+  {
+    "revision": "73a33ce032f7d452b952",
+    "url": "/static/js/2.4b5aee8c.chunk.js"
+  },
+  {
+    "revision": "e1f5ad522e1b15731a197887d22f1a58",
+    "url": "/static/js/2.4b5aee8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6096336fd90f41548a7",
+    "url": "/static/js/main.d6bf904b.chunk.js"
+  },
+  {
+    "revision": "71e0677ab2f28215227f",
+    "url": "/static/js/runtime-main.b234ba30.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
